cd322938f7b21431f835558dc184b2f2e7da687b
Dr. Takeyuki Ueda <grenouill.vert@gmail.com>
2016-08-03 11:57:47 +0000

    https if https now.

--- resources/views/unit/index.blade.php
+++ resources/views/unit/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>栽培棚管理</h1>
     <a href="/plant" class="btn btn-primary btn-sm">品種一覧</a>
-
+    {{Request::server('HTTP_X_FORWARDED_PROTO') == 'https'?1:2}}
     <hr/>
      
     <div class="container-fluid">
@@ -22,13 +22,14 @@
                         @foreach($unit->shelves as $shelf)
                             <tr>
                                 <td>
-                                    <a href="{{ url('shelf', $shelf->id) }}" data-ajax="false" >
+                                    <!-- <a href="{{ url('shelf', $shelf->id) }}" data-ajax="false" > -->
+                                    <a href="{{ route('shelf.show', [$shelf->id]) }}" data-ajax="false" >
                                       {{$shelf->name}}
                                     </a>
                                     <br>
                                     @foreach($shelf->plantings as $planting)
                                         @if(($planting->closed_at)==0)
-                                            <a href="{{ url('planting', $planting->id) }}" data-ajax="false" >
+                                            <a href="{{Request::server('HTTP_X_FORWARDED_PROTO') == 'https' ? secure_url('planting', $planting->id) : url('planting', $planting->id) }}" data-ajax="false" >
                                                 {{$planting->plant->name}}
                                             </a>
                                             <span class="badge pull-right">{{$planting->duration()}}</span><br>
